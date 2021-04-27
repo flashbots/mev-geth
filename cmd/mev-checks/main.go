e2d34ef81687bd7c55f61d2b83ee3b3f2966ee75
Edgar Aroutiounian <edgar.factorial@gmail.com>
2021-04-27 10:55:40 -0400

    [flashbots] Hardcode bin of known bribe contract

--- cmd/mev-checks/main.go
+++ cmd/mev-checks/main.go
@@ -6,6 +6,7 @@ import (
 	"flag"
 	"fmt"
 	"log"
+	"math/big"
 	"time"
 
 	"github.com/ethereum/go-ethereum/common"
@@ -15,17 +16,15 @@ import (
 )
 
 const (
-	miner              = "0xd912aecb07e9f4e1ea8e6b4779e7fb6aa1c3e4d8"
-	plainPayerContract = `
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.7.0;
-
-contract Bribe {
-    function bribe() payable public {
-        block.coinbase.transfer(msg.value);
-    }
-}
-`
+	miner = "0xd912aecb07e9f4e1ea8e6b4779e7fb6aa1c3e4d8"
+	// SPDX-License-Identifier: UNLICENSED
+	// pragma solidity ^0.7.0;
+	// contract Bribe {
+	//     function bribe() payable public {
+	//         block.coinbase.transfer(msg.value);
+	//     }
+	// }
+	bribeContractBin = `0x6080604052348015600f57600080fd5b5060a78061001e6000396000f3fe608060405260043610601c5760003560e01c806337d0208c146021575b600080fd5b60276029565b005b4173ffffffffffffffffffffffffffffffffffffffff166108fc349081150290604051600060405180830381858888f19350505050158015606e573d6000803e3d6000fd5b5056fea2646970667358221220862610b9326c9523da6465ba88229d2c6b26ff844b3c9cddb807c2c1ab401dd964736f6c63430007050033`
 )
 
 var (
@@ -39,12 +38,26 @@ var (
 	k1, _ = crypto.HexToECDSA(
 		"",
 	)
-	keys = []*ecdsa.PrivateKey{k1}
+	faucet, _ = crypto.HexToECDSA(
+		"133be114715e5fe528a1b8adf36792160601a2d63ab59d1fd454275b31328791",
+	)
+	keys = []*ecdsa.PrivateKey{k1, faucet}
 )
 
 func mbTxList() types.Transactions {
-	txs := make(types.Transactions, len(keys))
+	var txs types.Transactions
+	// test the contract creation way -
+	// also might be in megabundle right
+
+	t := types.NewContractCreation(
+		0, new(big.Int), 200_000, big.NewInt(10e9), common.Hex2Bytes(bribeContractBin),
+	)
+	t, _ = types.SignTx(t, types.NewEIP155Signer(big.NewInt(1)), faucet)
+	txs = append(txs, t)
+	// txs := make(types.Transactions, len(keys))
 	for i, key := range keys {
+		_ = i
+		_ = key
 		// txs[i] = types.NewTransaction(
 		// 		nonce uint64,
 		// 		to common.Address,
@@ -54,6 +67,7 @@ func mbTxList() types.Transactions {
 		// 		data []byte,
 		// 	)
 	}
+	// txs = append(txs, )
 	return txs
 }
 
