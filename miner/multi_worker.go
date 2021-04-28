8163e34356bc3f80669f6a966ed50a0a6ac6a112
Edgar Aroutiounian <edgar.factorial@gmail.com>
2021-04-28 09:19:32 -0400

    [flashbots] merge squash, fix conflict with new worker structure

--- miner/multi_worker.go
+++ miner/multi_worker.go
@@ -7,95 +7,101 @@ import (
 	"github.com/ethereum/go-ethereum/consensus"
 	"github.com/ethereum/go-ethereum/core/types"
 	"github.com/ethereum/go-ethereum/event"
+	"github.com/ethereum/go-ethereum/log"
 	"github.com/ethereum/go-ethereum/params"
 )
 
 type multiWorker struct {
-	regularWorker    *worker
-	flashbotsWorker  *worker
-	megaBundleWorker *worker
+	workers       []*worker
+	regularWorker *worker
 }
 
 func (w *multiWorker) stop() {
-	w.regularWorker.stop()
-	w.flashbotsWorker.stop()
-	w.megaBundleWorker.stop()
+	for _, worker := range w.workers {
+		worker.stop()
+	}
 }
 
 func (w *multiWorker) start() {
-	w.regularWorker.start()
-	w.flashbotsWorker.start()
-	w.megaBundleWorker.start()
+	for _, worker := range w.workers {
+		worker.start()
+	}
 }
 
 func (w *multiWorker) close() {
-	w.regularWorker.close()
-	w.flashbotsWorker.close()
-	w.megaBundleWorker.close()
+	for _, worker := range w.workers {
+		worker.close()
+	}
 }
 
 func (w *multiWorker) isRunning() bool {
-	return w.regularWorker.isRunning() ||
-		w.flashbotsWorker.isRunning() ||
-		w.megaBundleWorker.isRunning()
+	for _, worker := range w.workers {
+		if worker.isRunning() {
+			return true
+		}
+	}
+	return false
 }
 
 func (w *multiWorker) setExtra(extra []byte) {
-	w.regularWorker.setExtra(extra)
-	w.flashbotsWorker.setExtra(extra)
-	w.megaBundleWorker.setExtra(extra)
+	for _, worker := range w.workers {
+		worker.setExtra(extra)
+	}
 }
 
 func (w *multiWorker) setRecommitInterval(interval time.Duration) {
-	w.regularWorker.setRecommitInterval(interval)
-	w.flashbotsWorker.setRecommitInterval(interval)
-	w.megaBundleWorker.setRecommitInterval(interval)
+	for _, worker := range w.workers {
+		worker.setRecommitInterval(interval)
+	}
 }
 
 func (w *multiWorker) setEtherbase(addr common.Address) {
-	w.regularWorker.setEtherbase(addr)
-	w.flashbotsWorker.setEtherbase(addr)
-	w.megaBundleWorker.setEtherbase(addr)
+	for _, worker := range w.workers {
+		worker.setEtherbase(addr)
+	}
 }
 
 func (w *multiWorker) enablePreseal() {
-	w.regularWorker.enablePreseal()
-	w.flashbotsWorker.enablePreseal()
-	w.megaBundleWorker.enablePreseal()
+	for _, worker := range w.workers {
+		worker.enablePreseal()
+	}
 }
 
 func (w *multiWorker) disablePreseal() {
-	w.regularWorker.disablePreseal()
-	w.flashbotsWorker.disablePreseal()
-	w.megaBundleWorker.disablePreseal()
+	for _, worker := range w.workers {
+		worker.disablePreseal()
+	}
 }
 
 func newMultiWorker(config *Config, chainConfig *params.ChainConfig, engine consensus.Engine, eth Backend, mux *event.TypeMux, isLocalBlock func(*types.Block) bool, init bool) *multiWorker {
 	queue := make(chan *task)
 
+	regularWorker := newWorker(config, chainConfig, engine, eth, mux, isLocalBlock, init, &flashbotsData{
+		isFlashbots: false,
+		queue:       queue,
+	})
+
+	workers := []*worker{regularWorker}
+
+	for i := 1; i <= config.MaxMergedBundles; i++ {
+		workers = append(workers,
+			newWorker(config, chainConfig, engine, eth, mux, isLocalBlock, init, &flashbotsData{
+				isFlashbots:      true,
+				queue:            queue,
+				maxMergedBundles: i,
+			}))
+	}
+
+	log.Info("creating multi worker", "config.MaxMergedBundles", config.MaxMergedBundles, "worker", len(workers))
 	return &multiWorker{
-		regularWorker: newWorker(
-			config, chainConfig, engine, eth, mux, isLocalBlock, init, &flashbotsData{
-				queue: queue,
-			},
-		),
-		flashbotsWorker: newWorker(
-			config, chainConfig, engine, eth, mux, isLocalBlock, init, &flashbotsData{
-				isFlashbots: true,
-				queue:       queue,
-			},
-		),
-		megaBundleWorker: newWorker(
-			config, chainConfig, engine, eth, mux, isLocalBlock, init, &flashbotsData{
-				isMegaBundleWorker: true,
-				queue:              queue,
-			},
-		),
+		regularWorker: regularWorker,
+		workers:       workers,
 	}
 }
 
 type flashbotsData struct {
 	isFlashbots        bool
-	isMegaBundleWorker bool
 	queue              chan *task
+	maxMergedBundles   int
+	isMegaBundleWorker bool
 }
